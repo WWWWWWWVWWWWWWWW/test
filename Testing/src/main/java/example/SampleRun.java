24f305eeb3db8f8791e508cfc55f54218ce8f353
abc <a@a.a>
2015-10-09 00:29:24 -0400

    testing gitignore

---
+++ Testing/src/main/java/example/SampleRun.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main.java.example;
+
+/**
+ * @author Andy
+ *
+ */
+public class SampleRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey Wassup");
+	}
+
+}
